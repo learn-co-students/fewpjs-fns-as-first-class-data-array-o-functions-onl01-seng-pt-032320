aedaa1e2b0a14e78c03595f6c502645d8b7ce314
Shamel Lakin <christlife4u@gmail.com>
2020-09-27 15:34:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 function wakeDog(dogName, dogBreed){
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return(`Wake ${dogName} the ${dogBreed}`);
 }
 
 function leashDog(dogName, dogBreed){
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return(`Leash ${dogName} the ${dogBreed}`);
 }
 
 function walkToPark(dogName, dogBreed){
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return(`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 
 function throwFrisbee(dogName, dogBreed){
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return(`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
 function walkHome(dogName, dogBreed){
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return(`Walk home with ${dogName} the ${dogBreed}`);
 }
 
 function unleashDog(dogName, dogBreed){
-    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return(`Unleash ${dogName} the ${dogBreed}`);
 }
 
-
-let routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
-
-
-function exerciseDog(routine) {
+function arrayOFunctions(){
+let routine = ['wakeDog()', 'leashDog()', 'walkToPark()', 'throwFrisbee()', 'walkHome()', 'unleashDog()'];
     newArr = []
     for (let i = 0; i < routine.length; i++) {
         newArr.push(routine);
     }
-    return newArr;
+    return newArr
+ 
 }
-//  exerciseDog(sam, puggle);
+
+// function exerciseDog(dogName, dogBreed) {
+//     newArr = []
+//     for (let i = 0; i < routine.length; i++) {
+//         newArr.push(routine);
+//     }
+//     return newArr;
+// }
+//  exerciseDog("Sam", "puggle");
